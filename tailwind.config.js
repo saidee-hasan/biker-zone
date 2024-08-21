38f029ea4ca628af87d4c9ee9ce5c7ade1256300
saideeWD <mdsaideehasan@gmail.com>
2024-08-21 08:18:43 +0600

    header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['*'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "bike-primary":"rgba(231, 111, 81, 0.1)",
+        "btn-primary":"rgb(231, 111, 81)"
+      }
+    },
   },
   plugins: [require('daisyui'),],
 }
